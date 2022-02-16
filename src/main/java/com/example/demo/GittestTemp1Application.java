72e1e288006b659d7ef9dafeb907bf677b7c233b
USER1-PC\USER1 <jyi0480@naver.com>
2022-02-16 23:47:28 +0900

    초기버전

---
+++ src/main/java/com/example/demo/GittestTemp1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GittestTemp1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GittestTemp1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demo/GittestTemp1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GittestTemp1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
